54bcb55a5acce8b14624c2a2bae023caf98aa32c
Ava GianGrasso <agiangrasso@mail.wlu.edu>
2024-12-09 21:06:14 -0500

    minor graph edits

--- server.R
+++ server.R
@@ -45,14 +45,14 @@ function(input, output, session) {
   output$tempduration <- renderPlotly({
     if(input$plotType=="p"){
       plot2=ggplot(filtered_temp_data(), aes(Tempavg, Duration)) + geom_point (color="red") +
-        geom_smooth(method = "lm", color = "black", linewidth = 0.8, se = FALSE) +
+        geom_smooth(method = "loess", color = "black", linewidth = 0.8, se = FALSE) +
         labs(title="Average Temperature vs Ride Duration", x= "Average Temperature", y= "Average Ride Duration (mins)") +
         theme_minimal()
     }
     else if(input$plotType=="b"){
       plot2=ggplot(filtered_temp_data(), aes(x = Tempavg, y = Duration)) +
         geom_bar(stat = "identity", fill = "red") +
-        geom_smooth(method = "lm", color = "black", linewidth = 0.8, se = FALSE) +
+        geom_smooth(method = "loess", color = "black", linewidth = 0.8, se = FALSE) +
         labs(title = "Average Temperature vs Ride Duration", x = "Average Temperature", y = "Average Ride Duration (mins)") +
         theme_minimal()+
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
